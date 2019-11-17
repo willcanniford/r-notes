d78eee5800e737ff73d98ef845d16b80a50d1451
Will Canniford <willcanniford@yahoo.co.uk>
2019-11-17 11:04:35 +0000

    Add script that generates tailwind_scales.jpeg

---
+++ images/tailwind_scales.R
@@ -0,0 +1,21 @@
+library(devtools)
+library(tidyverse)
+
+# Install R package from github
+devtools::install_github('willcanniford/ggtailwind')
+
+# Generate colour palette visual
+tibble(fullname = names(ggtailwind::tailwind_cols_regex('.*-.*'))) %>%
+  rowwise() %>%
+  mutate(
+    hex = ggtailwind::tailwind_cols(fullname),
+    base_colour = as_factor(strsplit(fullname, '-')[[1]][1]),
+    shade = as_factor(strsplit(fullname, '-')[[1]][2])
+  ) %>%
+  ggplot(aes(shade, base_colour)) +
+  geom_tile(aes(fill = hex)) +
+  scale_fill_identity(guide = F) +
+  theme_minimal() +
+  coord_equal() +
+  labs(x = NULL, y = NULL) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
