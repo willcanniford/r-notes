85a01e36e5b8d42236cf25f6bec07e747b50e443
Will Canniford <willcanniford@yahoo.co.uk>
2021-01-27 12:34:45 +0000

    Update tailwind scales image

--- images/tailwind_scales.R
+++ images/tailwind_scales.R
@@ -12,10 +12,12 @@ tibble(fullname = names(ggtailwind::tailwind_cols_regex('.*-.*'))) %>%
     base_colour = as_factor(strsplit(fullname, '-')[[1]][1]),
     shade = as_factor(strsplit(fullname, '-')[[1]][2])
   ) %>%
-  ggplot(aes(shade, base_colour)) +
+  ggplot(aes(fct_reorder(shade, as.numeric(shade)), base_colour)) +
   geom_tile(aes(fill = hex)) +
   scale_fill_identity(guide = F) +
   theme_minimal() +
   coord_equal() +
   labs(x = NULL, y = NULL) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) -> scales_graphic
+
+ggsave(scales_graphic, filename = 'tailwind_scales.jpeg')
